907f1f29c239c53cb9ec1ffbcc3533c571aa30d0
bagger3025 <bagger3025@g.skku.edu>
2023-08-12 22:11:01 +0900

    no multer 해결
    이슈 #1

--- src/app/_submitComponent.js
+++ src/app/_submitComponent.js
@@ -1,28 +1,28 @@
-"use client"
+"use client";
 
-export default function FileSubmit(){
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    let formData = new FormData();
-    console.log(e.target.photo.files[0]);
-    console.log(e.target.photo.files[0]);
-    formData.append('photo', e.target.photo.files[0]);
-    console.log(formData.getAll("photo"));
-    const resp = await fetch("http://localhost:3000/api", {
-      method: "POST",
-      body: formData
-    });
-    if (!resp.ok){
-      console.log("fetch failed");
-      return;
-    }
-    const result = await resp.json();
-    console.log(result);
-  }
-  return (
-  <form onSubmit={onSubmit}>
-    <input type="file" name="photo"/>
-    <input type="submit" value="제출"/>
-  </form>
-  );
+export default function FileSubmit() {
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		let formData = new FormData();
+		console.log(e.target.photo.files[0]);
+		console.log(e.target.photo.files[0]);
+		formData.set("photo", e.target.photo.files[0]);
+		console.log(formData.getAll("photo"));
+		const resp = await fetch("http://localhost:3000/api", {
+			method: "POST",
+			body: formData,
+		});
+		if (!resp.ok) {
+			console.log("fetch failed");
+			return;
+		}
+		const result = await resp.json();
+		console.log(result);
+	};
+	return (
+		<form onSubmit={onSubmit}>
+			<input type="file" name="photo" />
+			<input type="submit" value="제출" />
+		</form>
+	);
 }
--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,6 +1,13 @@
 import { createRouter, expressWrapper } from "next-connect";
+import { writeFile } from "fs/promises";
 import multer from "multer";
 import { NextResponse } from "next/server";
+
+export const config = {
+	api: {
+		bodyParser: true,
+	},
+};
 
 const upload = multer({
 	storage: multer.diskStorage({
@@ -31,13 +38,25 @@ export function GET(req, ctx) {
 	return router.run(req, ctx);
 }
 
-export function POST(req, ctx) {
-	console.log(req);
-	return router.run(req, ctx);
-}
+export async function POST(request) {
+	console.log(request);
+	const data = await request.formData();
 
-export const config = {
-	api: {
-		bodyParser: true,
-	},
-};
+	console.log(data);
+	const file = data.get("photo");
+
+	if (!file) {
+		return NextResponse.json({ success: false });
+	}
+
+	const bytes = await file.arrayBuffer();
+	const buffer = Buffer.from(bytes);
+
+	// With the file data in the buffer, you can do whatever you want with it.
+	// For this, we'll just write it to the filesystem in a new location
+	const path = `./tmp/${file.name}`;
+	await writeFile(path, buffer);
+	console.log(`open ${path} to see the uploaded file`);
+
+	return NextResponse.json({ success: true });
+}
